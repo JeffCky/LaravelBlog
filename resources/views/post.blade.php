c58489eaa6b691e9d529b51503da5fc726e71ad4
JeffCky <104943706+JeffCky@users.noreply.github.com>
2022-05-05 09:51:56 -0400

    Added Categories
    Added Categories Table and to Posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
     <h1> {{$post->title;}} </h1>
+    <p>
+        <h4>Category: <a href="#">{{$post->category->name}}</a></h4>
+    </p>
     <article>
          {!!$post->body;!!}
     </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,9 +2,13 @@
     @foreach ($posts as $post)
         <article>
            <h1>
-               <a href="/posts/{{$post->id;}}">{{ $post->title;}}</a>
+               <a href="/posts/{{$post->slug;}}">{{ $post->title;}}</a>
 
             </h1>
+
+            <p>
+                <h4>Category: <a href="#">{{$post->category->name}}</a></h4>
+            </p>
 
            <div>{{$post->excerpt;}}</div>
         </article>
