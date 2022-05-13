55781a98c358883ccf6e9e5621ae7beefe8b952a
JeffCky <jeffcarterky@gmail.com>
2022-05-13 17:33:32 -0400

    Formatting changes to blog and wiring functionality

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,25 @@
 <x-layout>
-    @foreach ($posts as $post)
-        <article>
-           <h1>
-               <a href="/posts/{{$post->slug;}}">{{ $post->title;}}</a>
 
-            </h1>
-            <a href="/authors/{{$post->author->username}}"><h4>By {{$post->author->name}}</h4></a>
 
-            <p>
-                <h4>Category: <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></h4>
-            </p>
+    @include('_posts-header')
 
-           <div>{{$post->excerpt;}}</div>
-        </article>
-    @endforeach
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+
+            <x-post-grid :posts="$posts" />
+
+        @else
+            <p class="text-center">There are no posts at this time. Please check back later.</p>
+        @endif
+
+
+
+
+
+
+        </div>
+
+
+    </main>
 
 </x-layout>
-
